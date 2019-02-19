5d4a5f3e4e5faa18d0627bcf3a808e180fcef307
philippe-nougailon <philippe.nougaillon@gmail.com>
2019-02-19 14:28:17 +0100

    planning selector crash when cours null fixed!

--- src/selectors/planning.js
+++ src/selectors/planning.js
@@ -1,10 +1,10 @@
 const getFiltredPlanning = (planning, filter) =>
 
     planning.filter(cours => 
-                cours.formation_json_v2.toLowerCase().includes(filter) ||
-                cours.intervenant_json.toLowerCase().includes(filter) ||
-                cours.salle_json_v2.toLowerCase().includes(filter)
-                );
+                    cours.formation_json_v2.toLowerCase().includes(filter) ||
+                    cours.intervenant_json.toLowerCase().includes(filter)  ||
+                    (cours.salle_json_v2 && cours.salle_json_v2.toLowerCase().includes(filter))
+                    );
 
 export {
     getFiltredPlanning,
