166cde281d1366b4e698a46d6db81fa2c34f4929
philippe-nougailon <philippe.nougaillon@gmail.com>
2019-02-20 17:21:27 +0100

    Grid & Typo

--- src/components/Cours.js
+++ src/components/Cours.js
@@ -1,4 +1,9 @@
 import React from 'react';
+
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import 'typeface-roboto';
+
 import './Cours.css';
 
 const Cours = ({ cours }) => {
@@ -12,21 +17,33 @@ const Cours = ({ cours }) => {
 
     return(
         <div className="cours">
-            <span style= {{ width: '7%' }}>
-                { debut_fin_json_v2 }
-            </span>
-            <span style= {{ width: '30%' }}>
-                { formation_json_v2 }
-            </span>
-            <span  style= {{ width: '33%', fontWeight: 'lighter' }}>
-                { matiere_json }
-            </span>
-            <span style= {{ width: '20%', fontWeight: 'lighter' }} >
-                { intervenant_json }
-            </span>
-            <span style= {{ width: '10%', fontWeight: 'lighter' }} >
-                { salle_json_v2 }
-            </span>
+            <Grid container spacing={5}>
+                <Grid item xs={3} sm={1}>
+                    <Typography>
+                        <b>{ debut_fin_json_v2 }</b>
+                    </Typography>
+                </Grid>
+                <Grid item xs={6} sm={3}>
+                    <Typography>
+                        <b>{ formation_json_v2 }</b>
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} sm={2}>
+                    <Typography>
+                        { intervenant_json }
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} sm={5} zeroMinWidth>
+                    <Typography noWrap>
+                        <i>{ matiere_json }</i>
+                    </Typography>
+                </Grid>
+                <Grid item xs={3} sm={1}>
+                    <Typography color="primary">
+                        <b>{ salle_json_v2 }</b>
+                    </Typography>
+                </Grid>
+            </Grid>
         </div>
     )
 }
