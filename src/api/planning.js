0e832b405a84dbbf0c8330330e4d22db76c48773
philippe-nougailon <philippe.nougaillon@gmail.com>
2019-02-26 15:25:19 +0100

    api url prod

--- src/api/planning.js
+++ src/api/planning.js
@@ -1,7 +1,7 @@
-const BASE_URL = 'https://iae-test.philnoug.com/api/v2/cours.json?d=';
+const BASE_URL = 'https://planning.iae-paris.com/api/v2/cours.json?d=';
 
+// const BASE_URL = 'https://iae-test.philnoug.com/api/v2/cours.json?d=';
 // const BASE_URL = 'http://localhost:3030/api/v2/cours.json?d=';
-
 
 const fetchPlanning = query => 
         fetch(BASE_URL + query)
--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,6 @@ import './App.css';
 
 import AppBar from './AppBar';
 
-
 import Planning from './Planning';
 import SearchPlanning from './SearchPlanning';
 
