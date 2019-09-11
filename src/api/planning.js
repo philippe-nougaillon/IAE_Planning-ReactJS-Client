fe3bc30576c5d09c1994f81d5935799c8a47df56
philippe-nougailon <philippe.nougaillon@gmail.com>
2019-09-11 12:31:50 +0200

    test url

--- src/api/planning.js
+++ src/api/planning.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'http://localhost:3030/';
-//const BASE_URL = 'https://iae-test.philnoug.com/';
+//const BASE_URL = 'http://localhost:3030/';
+const BASE_URL = 'https://iae-test.philnoug.com/';
 //const BASE_URL = 'https://planning.iae-paris.com/';
 
 const REQUEST_URL = BASE_URL + "api/v2/cours.json?d=";
