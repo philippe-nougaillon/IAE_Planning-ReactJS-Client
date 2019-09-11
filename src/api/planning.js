8dc2fcc68e7728ce47455a7c9d4aa9f4e03e3447
philippe-nougailon <philippe.nougaillon@gmail.com>
2019-09-11 23:25:42 +0200

    prod url

--- src/api/planning.js
+++ src/api/planning.js
@@ -1,6 +1,6 @@
 //const BASE_URL = 'http://localhost:3030/';
-const BASE_URL = 'https://iae-test.philnoug.com/';
-//const BASE_URL = 'https://planning.iae-paris.com/';
+//const BASE_URL = 'https://iae-test.philnoug.com/';
+const BASE_URL = 'https://planning.iae-paris.com/';
 
 const REQUEST_URL = BASE_URL + "api/v2/cours.json?d=";
 
