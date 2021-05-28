4140ed505c9a52a756f973cc68d7f65be93e60b0
Philippe Nougaillon <philippe.nougaillon@gmail.com>
2021-05-28 15:40:08 +0200

    serveur url modifiée

--- src/api/planning.js
+++ src/api/planning.js
@@ -1,7 +1,7 @@
 // const BASE_URL = 'http://localhost:3030/';
 // const BASE_URL = 'https://iae-test.philnoug.com/';
 // const BASE_URL = 'https://planning.iae-paris.com/';
-const BASE_URL = 'https://planning4-demo.herokuapp.com/';
+const BASE_URL = 'https://planning-iae.herokuapp.com/';
 
 const REQUEST_URL = BASE_URL + "api/v2/cours.json?d=";
 
